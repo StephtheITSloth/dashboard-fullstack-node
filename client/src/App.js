1896f3289452b3afc8fd65fa792c34794ea8a0b3
stephtheitsloth <stephan.karim.sk@gmail.com>
2023-10-17 05:34:23 -0400

    create theme slice + configure store + setup mui theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import {createTheme} from "@mui/material";
+import { themeSettings } from "theme";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+
 
 function App() {
+  const mode = useSelector(state => state.global.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
   return (
     <div className="App">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   );
 }
